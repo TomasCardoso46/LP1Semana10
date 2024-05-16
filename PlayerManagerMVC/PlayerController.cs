ce201e67e7f82bc1ba2510b99459794899962f9a
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-16 23:03:25 +0100

    Added PlayerController.cs

---
+++ PlayerManagerMVC/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using PlayerManagerMVC.Models;
+
+namespace PlayerManagerMVC.Controllers
+{
+    public class PlayerController
+    {
+        private List<Player> _players;
+
+        public PlayerController()
+        {
+            _players = new List<Player>
+            {
+                new Player("Player1", 100),
+                new Player("Player2", 200)
+            };
+        }
+
+        public void InsertPlayer(string name, int score)
+        {
+            _players.Add(new Player(name, score));
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return _players;
+        }
+
+        public List<Player> GetPlayersWithScoreGreaterThan(int minScore)
+        {
+            return _players.FindAll(player => player.Score > minScore);
+        }
+
+        public List<Player> GetPlayersSortedByName(bool ascending)
+        {
+            var comparer = new CompareByName(ascending);
+            _players.Sort(comparer);
+            return _players;
+        }
+    }
+}
