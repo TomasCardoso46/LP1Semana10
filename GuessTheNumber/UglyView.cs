6e0ffd162c0bde0139d4daf4e6021e54455f1ce9
TomasCardoso46 <tomascardoso4633@gmail.com>
2024-05-17 16:27:54 +0100

    Added all the Console.Writelines to its respective UglyView method

--- GuessTheNumber/UglyView.cs
+++ GuessTheNumber/UglyView.cs
@@ -10,17 +10,31 @@ namespace GuessTheNumber
         private readonly Controller controller;
 
 
-     public void HigherNumber()
+    public void StartMessage()
         {
-
+            Console.WriteLine("Welcome to Guess the Number!");
+            Console.WriteLine("I have chosen a number between 1 and 100.");
+        }
+    public void TakeAGuess()
+        {
+            Console.WriteLine("Take a guess!");
+        }
+    public void HigherNumber()
+        {
+            Console.WriteLine("Too high! Try again.");
         }
     public void LowerNumber()
         {
-
+            Console.WriteLine("Too low! Try again.");
         }
     public void CorrectNumber()
         {
-            
+           Console.WriteLine("Congratulations! You guessed the number correctly!");
+           Console.WriteLine("Number of attempts: " + attempts); 
+        }
+    public void EndMessage()
+        {
+            Console.WriteLine("Thank you for playing Guess the Number!");
         }
 
     }
